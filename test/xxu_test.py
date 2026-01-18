ca8176272fed655b777db830f93e43eda4789708
Diego Frias <mail@dzfrias.dev>
2026-01-18 09:22:29 -0600

    Implement test extending
    Some test inputs are small enough that the scalar implementation is used
    for the whole test. We now artificially extend small tests so that SIMD
    paths can be activated (note that we still test the original as well).

--- test/xxu_test.py
+++ test/xxu_test.py
@@ -4,25 +4,36 @@ import subprocess
 import sys
 from pathlib import Path
 
+
 ALGORITHMS = ["nfd", "nfkd"]
+
+
+def compare(xxu_bin, algorithm, name, contents):
+    xxu_result = subprocess.run(
+        [xxu_bin, "-x", algorithm], capture_output=True, input=contents
+    )
+    if xxu_result.returncode != 0:
+        print(f"got exit code {xxu_result.returncode} for input {name}")
+        sys.exit(1)
+    uconv_result = subprocess.run(
+        ["uconv", "-x", f"any-{algorithm}"], capture_output=True, input=contents
+    )
+    assert uconv_result.returncode == 0
+    if xxu_result.stdout != uconv_result.stdout:
+        print(f"got output mismatch for input {name}")
+        sys.exit(1)
+
 
 xxu_bin = sys.argv[1]
 
-for algorithm in ALGORITHMS:
-    for input_dir in sys.argv[2:]:
-        for file in Path(input_dir).iterdir():
-            xxu_result = subprocess.run(
-                [xxu_bin, "-x", algorithm, str(file)], capture_output=True
-            )
-            if xxu_result.returncode != 0:
-                print(f"got exit code {xxu_result.returncode} for input {str(file)}")
-                sys.exit(1)
-            uconv_result = subprocess.run(
-                ["uconv", "-x", f"any-{algorithm}", str(file)], capture_output=True
-            )
-            assert uconv_result.returncode == 0
-            if xxu_result.stdout != uconv_result.stdout:
-                print(f"got output mismatch for input {str(file)}")
-                sys.exit(1)
+for input_dir in sys.argv[2:]:
+    for file in Path(input_dir).iterdir():
+        contents = file.read_bytes()
+        for algorithm in ALGORITHMS:
+            name = str(file)
+            compare(xxu_bin, algorithm, name, contents)
+            if len(contents) < 256:
+                extended = contents + b"0" * (256 - len(contents))
+                compare(xxu_bin, algorithm, f"EXTENDED-{name}", extended)
 
 print("All xxu tests passed!")
